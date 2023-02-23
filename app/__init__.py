c36c5e3e6a3111848f5ca4c175340d536c68d73b
Sedykh Vitaliy <sedykhvit@gmail.com>
2023-02-23 15:05:21 +0300

    #4 update bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-bootstrap = Bootstrap4(app)
+bootstrap = Bootstrap(app)
 
 with app.app_context():
     db.create_all()
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "fdsfnvudfvdnru"
